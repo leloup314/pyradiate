808d01fa5da80a15c0c87b3612f112835129c53a
leloup314 <pascal.wolf@posteo.de>
2026-06-14 00:32:07 +0200

    MAINT: add __init__ file for core and export main classes

---
+++ pyradiate/core/__init__.py
@@ -0,0 +1,6 @@
+from .nuclide_id import NuclideIdentifier
+from .elements import Elements
+from .radio_nuclide import RadioNuclide
+from .sources import Source, CalibratedSource
+
+__all__ = ["NuclideIdentifier", "Elements", "RadioNuclide", "Source", "CalibratedSource"]
